c6cb0aeb5fe30d4c19ff4a701daae96fd61f22c6
Sarthak Nigam <srthkngm@gmail.com>
2024-03-06 15:40:02 +0530

    intital setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))  //this means that i am accepting json data 
+app.use(express.urlencoded({extended:true,limit:"16kb"})) //in url , space is converted into + or % thsi is done by urlencoded
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export { app };
