44ed92ae70c00034dba1e11284447735be96f124
Sarthak Nigam <srthkngm@gmail.com>
2024-03-07 20:42:40 +0530

    Fix validation bug in loginUser function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -88,7 +88,7 @@ const loginUser = asyncHandler( async (req,res) => {
 
     const {email,password,username,avatar} = req.body
 
-    if (!(username || email)) {
+    if (!(username || email || password)) {
         throw new ApiError(400,"username or email is required!!")
     }
 
